b3a57adc7d40f18b0a2a646b983bed2293e6c424
Mudavath-Swathi <swathichauhan22@gmail.com>
2026-03-05 23:36:34 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://churnguard-saas-dashboard.onrender.com",
+  baseURL: "https://churnguard-saas-dashboard.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
